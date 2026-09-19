6a5996b0e6d946c5582cea0fd1b6058899eb9e90
anajulia0407 <anajulialindholm2@gmail.com>
2026-09-19 07:33:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    let icon = document.querySelector('.icon');
-
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        icon.src = "imagens/menu_white_36dp (1).svg";
-    } else {
-        menuMobile.classList.add('open');
-        icon.src = "imagens/close_white_36dp.svg";
-    }
-}
-
-
 let count = 1;
 document.getElementById("radio1").checked = true;
 
